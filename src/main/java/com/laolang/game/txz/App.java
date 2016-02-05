ef2b7da1307961726c8e661ec359960a870fc01d
laolang2016 <xiaodaima20162163.com>
2016-02-05 19:35:26 +0800

    完成：     1、添加项目

---
+++ src/main/java/com/laolang/game/txz/App.java
@@ -0,0 +1,13 @@
+package com.laolang.game.txz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
